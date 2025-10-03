b31c1becd22c19705f4a31c5c2d688db2d62200e
SundaramDev <sundaramkumar75655@gmail.com>
2025-10-04 01:04:36 +0530

    Modify all router

--- init/index.js
+++ init/index.js
@@ -1,22 +1,23 @@
-const mongoose=require("mongoose");
-const initdata=require("./data.js");
-const Listing=require("../models/listing.js");
-const mongo_url="mongodb://127.0.0.1/wanderlust";
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+const mongo_url = "mongodb://127.0.0.1/wanderlust";
 
 main()
-    .then(()=>{
-          console.log("connection established");
+    .then(() => {
+        console.log("connection established");
+        initDB(); // call initDB after connection
     })
-    .catch((err)=>{
-          console.log(err);
+    .catch((err) => {
+        console.log(err);
     });
-async function main(){
+
+async function main() {
     await mongoose.connect(mongo_url);
-};
-const initDB=async()=>{
+}
+
+const initDB = async () => {
     await Listing.deleteMany({});
     await Listing.insertMany(initdata.data);
     console.log("data was initialised");
-;}
-
-initDB();
+};
