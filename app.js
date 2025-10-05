feb7be17bc5c5dfc4796b7dba33f00d7ffef8d55
SundaramDev <sundaramkumar75655@gmail.com>
2025-10-05 16:16:06 +0530

    Fix wrapAsync implementation and update listing routes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 async function main() {
@@ -50,11 +51,12 @@ app.get("/listings/:id", async (req, res) => {
 });
 
 //Create Route
-app.post("/listings", async(req, res)=>{
-      const newListing = new Listing(req.body.listing);
-      await newListing.save();
-      res.redirect("/listings");
-})
+app.post("/listings", wrapAsync(async (req, res, next) => {
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+}));
+
 // Edit Route
 
 app.get("/listings/:id/edit", async (req, res) => {
@@ -93,6 +95,12 @@ app.delete("/listings/:id", async(req, res)=>{
 //     console.log("Sample was saved");
 //     res.send("Successful testing");
 // });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong! " + err.message);
+});
+
 
 app.listen(3000, () => {
     console.log("Server listening to port 3000");
--- models/listing.js
+++ models/listing.js
@@ -15,10 +15,10 @@ const listingSchema = new Schema({
         url: {
             type: String,
             default:
-                "C:/Users/gamin/OneDrive/Desktop/first.jpg",
+                "https://imgs.search.brave.com/SeUfc9cBP9Mwyqca9vkjDXc0b6Bnn9IsYp7VtZjEe_M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxOC8w/MS8xMi8xNC8yNC9u/aWdodC0zMDc4MzI2/XzEyODAuanBn",
             set: (v) =>
                 v === ""
-                    ? "C:/Users/gamin/OneDrive/Desktop/first.jpg"
+                    ? "https://imgs.search.brave.com/SeUfc9cBP9Mwyqca9vkjDXc0b6Bnn9IsYp7VtZjEe_M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxOC8w/MS8xMi8xNC8yNC9u/aWdodC0zMDc4MzI2/XzEyODAuanBn"
                     : v,
         },
     },
